2ed53a1cb1a4085e73be1f503bf400411231d1bd
Kostetrakh <kostetrakh@gmail.com>
2021-10-01 19:23:09 +0700

    init

---
+++ src/main/java/SerializationService.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class SerializationService {
+
+    public String serializeProductAsJSON(Product product) throws Exception {
+
+        // you should implement
+        return null;
+    }
+
+    public Product deserializeProductFromJSON(String str) throws Exception {
+
+        // you should implement
+        return null;
+    }
+}
